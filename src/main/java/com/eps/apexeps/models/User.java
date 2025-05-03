37d0c081a4b5266842aabfc5f418d3bf4b72b231
SC0C045 <St2ven.cabarcas@gmail.com>
2025-05-02 19:15:48 -0500

    feat: Endpoints api/users adaptado a la BD

--- src/main/java/com/eps/apexeps/models/User.java
+++ src/main/java/com/eps/apexeps/models/User.java
@@ -2,32 +2,32 @@ package com.eps.apexeps.models;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "users")
+@Table(name = "paciente")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // o SEQUENCE si Supabase lo requiere
     private Long id;
 
-    private String name;
-    private String email;
+    private String nom_paciente;
+    private String email_paciente;
 
     // Constructores
     public User() {}
-    public User(String name, String email) {
-        this.name = name;
-        this.email = email;
+    public User(String nom_paciente, String email_paciente) {
+        this.nom_paciente = nom_paciente;
+        this.email_paciente = email_paciente;
     }
 
     // Getters y Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getName() { return nom_paciente; }
+    public void setName(String nom_paciente) { this.nom_paciente = nom_paciente; }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() { return email_paciente; }
+    public void setEmail(String email) { this.email_paciente = email_paciente; }
 }
 
 
